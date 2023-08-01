44724abdb12f9438560ea22616f4df984f27f6c9
ebcho <joeunbi514@naver.com>
2023-08-01 22:21:28 +0900

    Initial commit

---
+++ src/main/java/com/ebcho/swimtalk/SwimtalkApplication.java
@@ -0,0 +1,13 @@
+package com.ebcho.swimtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimtalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebcho/swimtalk/SwimtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebcho.swimtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
