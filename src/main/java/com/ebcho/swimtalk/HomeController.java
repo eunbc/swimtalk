5ab5b5ff46b76a853f0b4a44a0c4b46154d6dedc
ebcho <joeunbi514@naver.com>
2023-08-11 12:33:19 +0900

    날짜 형식 변경

--- src/main/java/com/ebcho/swimtalk/HomeController.java
+++ src/main/java/com/ebcho/swimtalk/HomeController.java
@@ -2,9 +2,6 @@ package com.ebcho.swimtalk;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +20,7 @@ public class HomeController {
 		@RequestParam(defaultValue = "0") int page,
 		@RequestParam(defaultValue = "10") int size) {
 		Page<Post> postPage = postService.getAllPosts(page,size);
-		model.addAttribute("posts", postPage.getContent());
-		model.addAttribute("currentPage", postPage.getNumber());
-		model.addAttribute("totalPages", postPage.getTotalPages());
+		model.addAttribute("posts", postPage);
 		return "index";
 	}
 
